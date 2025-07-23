020608b2272bc24274db393828afa014c03bd724
Griffin <griffinnnnn77@gmail.com>
2025-07-23 10:30:08 +0300

    Fix OpenAI SDK usage with client instance

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from flask_mail import Mail, Message
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
