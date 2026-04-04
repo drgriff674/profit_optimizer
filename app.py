2a69b95d1449a7eb20800c6422bb1df9d963d8b6
Griffin <griffinnnnn77@gmail.com>
2026-04-04 14:15:26 +0300

    Import csrf exempt

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import (
     send_file,
     jsonify,
 )
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect, csrf_exempt
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask import Response
